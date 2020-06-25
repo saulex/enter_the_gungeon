711d25db3985e88efc4bc90b65ef9ca9bef3f65a
Air <airmanhigh@gmail.com>
2020-06-25 16:51:15 +0800

    init class: Enemy

---
+++ Classes/Enemy.hpp
@@ -0,0 +1,29 @@
+#ifndef _ETG_ENEMY_HPP_
+#define _ETG_ENEMY_HPP_
+
+#include <Character.hpp>
+#include <Status.hpp>
+#include <config.hpp>
+#include <map>
+#include <my_utils.hpp>
+#include <string>
+
+#include "cocos2d.h"
+
+using namespace cocos2d;
+
+namespace etg {
+
+class Enemy : public Character {
+public:
+    CREATE_FUNC(Enemy);
+    Enemy();
+    static Enemy* create(const std::string& filename);
+public:
+    bool init() override;
+    void update(float delta) override;;
+};
+
+}
+
+#endif
